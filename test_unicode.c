f92ec3d6100e7ed75d367f249b20694ba0e8954e
Tanveer Salim <tanveerasalim@github.com>
2019-04-08 17:03:01 -0500

    master

---
+++ test_unicode.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+	const uint8_t * locale = setlocale(LC_ALL,"");
+
+	printf("The current locale is %s\n",locale);
+
+	printf("\u00b7");
+
+}
