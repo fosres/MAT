452428ae0540a416df90662159d2a4bd05b15c84
Tanveer Salim <tanveerasalim@github.com>
2019-04-15 14:41:37 -0500

    master

--- test_unicode.c
+++ test_unicode.c
@@ -1,23 +1,76 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include <locale.h>
-#include <wchar.h>
+//#include <wchar.h>
+#include <iconv.h>
+
+void reverse(unsigned char s[])
+{
+  for (int i = 0, j = strlen(s)-1; i < j; i++, j--)
+  {
+    unsigned char temp = s[i];
+
+    s[i] = s[j];
+
+    s[j] = temp;
+  }
+}
 
 int main(void)
 {
-	const uint8_t * locale = setlocale(LC_ALL,"ar_EG.UTF-8");
+	const uint8_t * locale = setlocale(LC_ALL,"");
 
-	uint8_t s[5] = {0x00,0x00,0xfd,0xf0,0x00};
+	//printf("The locale is %s\n",locale);
 
-	//printf("\U000000ae");
+	uint32_t utf8_hex = 0xe1bea8;	
+	
+	uint32_t utf8_space = 0x20;
 
+	
+	static uint8_t s[5];
+	
+	memcpy(s,&utf8_hex,sizeof(uint32_t));
+
+	reverse(s);
+
+#if 0	
 	rsize_t i = 0;
 
-	while ( i < 10 )
+	while ( i < 5 )
 	{
-		printf("%c",0x82);
+		printf("%.02x",s[i]);
 
 		i++;
 	}
-	
+
+	putchar(0xa);
+#endif
+	printf("%s",s);
+
+//	memset_s(s,5*sizeof(uint8_t),0x0,5*sizeof(uint8_t));
+
+	memcpy(s,&utf8_space,sizeof(uint32_t));	
+
+	reverse(s);
+
+//	printf("%s",s);
+
+//	memset_s(s,5*sizeof(uint8_t),0x0,5*sizeof(uint8_t));
+
+	memcpy(s,&utf8_hex,sizeof(uint32_t));
+
+	reverse(s);
+
+	printf("%s",s);
+
+	printf("%s",s);
+
+	printf("%s",s);
+
+	printf("%s",s);
+
+	printf("%s",s);
+
 	return 0;
 }
